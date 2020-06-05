0acad4c71378b7f41cd1702af1aefa7378e8ee14
Adhishreya <adhishreyas@gmail.com>
2020-06-05 21:44:22 +0530

    Updates

--- precache-manifest.7543bc81dff8f03797624bebf598b244.js
+++ precache-manifest.7543bc81dff8f03797624bebf598b244.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d51be6dab3d98cfb084af33987b3d95a",
+    "revision": "3515136e517cb9c3e13a36039a82f2cb",
     "url": "/covid19track/index.html"
   },
   {
-    "revision": "492aa0212139e31d8a4c",
+    "revision": "2d70ab7345850f218b94",
     "url": "/covid19track/static/css/main.5b8798de.chunk.css"
   },
   {
-    "revision": "e3f225c0cde3a9c63c8e",
-    "url": "/covid19track/static/js/2.633fbe31.chunk.js"
+    "revision": "e62a36703239f66f60db",
+    "url": "/covid19track/static/js/2.3ed49cc3.chunk.js"
   },
   {
     "revision": "2903f65e346887768e2d109a63fdd7a4",
-    "url": "/covid19track/static/js/2.633fbe31.chunk.js.LICENSE.txt"
+    "url": "/covid19track/static/js/2.3ed49cc3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "492aa0212139e31d8a4c",
-    "url": "/covid19track/static/js/main.fd1d93ad.chunk.js"
+    "revision": "2d70ab7345850f218b94",
+    "url": "/covid19track/static/js/main.bba333e5.chunk.js"
   },
   {
     "revision": "7647620b3e8c83d77e5d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19track/precache-manifest.1e2b0088601934fdb137f79bd480c9fc.js"
+  "/covid19track/precache-manifest.7543bc81dff8f03797624bebf598b244.js"
 );
 
 self.addEventListener('message', (event) => {
