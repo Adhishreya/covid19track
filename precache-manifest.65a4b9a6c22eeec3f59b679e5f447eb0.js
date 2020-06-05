a36d82801a4258eb32935880b0f2d4bd257bd7f2
Adhishreya <adhishreyas@gmail.com>
2020-06-05 20:51:51 +0530

    Updates

---
+++ precache-manifest.65a4b9a6c22eeec3f59b679e5f447eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44b894e70d368fdde62a7a57a2806b5",
+    "url": "/covid19track/index.html"
+  },
+  {
+    "revision": "fa117b64eb17215194ea",
+    "url": "/covid19track/static/css/main.5b8798de.chunk.css"
+  },
+  {
+    "revision": "e3f225c0cde3a9c63c8e",
+    "url": "/covid19track/static/js/2.633fbe31.chunk.js"
+  },
+  {
+    "revision": "2903f65e346887768e2d109a63fdd7a4",
+    "url": "/covid19track/static/js/2.633fbe31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa117b64eb17215194ea",
+    "url": "/covid19track/static/js/main.968c811f.chunk.js"
+  },
+  {
+    "revision": "7647620b3e8c83d77e5d",
+    "url": "/covid19track/static/js/runtime-main.3a8f78fe.js"
+  }
+]);
