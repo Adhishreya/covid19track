376aec0c9fa0404c3c4e3a7e58f81efae0a25168
Adhishreya P <42477290+Adhishreya@users.noreply.github.com>
2021-07-12 00:22:23 +0530

    readme.md

---
+++ Readme.md
@@ -0,0 +1,18 @@
+covid-19track
+
+<!-- ### cases
+
+- https://api.covid19india.org/documentation/
+
+### Vaccination api
+
+- https://api.cowin.gov.in/api/v1/reports/v2/getVacPublicReports?state_id=&district_id=&date=2021-07-12
+
+- https://www.mygov.in/sites/default/files/covid/vaccine/covid_vaccine_timeline.json?#time.to_i%20-o%20./vdata.#{stim}%60
+
+### other data
+
+- https://covidtracking.com/data/
+- https://projects.datameet.org/covid19/icmr/
+- https://datascience.nih.gov/covid-19-open-access-resources
+- https://www.programmableweb.com/api/covid-19india-rest-api-v10 -->
--- src/Statewise.js
+++ src/Statewise.js
@@ -25,12 +25,32 @@ const StateWise = () => {
       .then((res) => {
         // console.log(res.statewise);
         res.statewise.map((data) => {
-          setKeys(Object.keys(data));
-          result.push(Object.values(data));
+          let keys = Object.keys(data);
+          // console.log(keys);
+          let keyArrs = keys.splice(0, 3);
+          keyArrs.push("recovered");
+          keyArrs.push("state");
+
+          setKeys(keyArrs);
+
+          let vals = Object.values(data);
+          // console.log(vals);
+          let valArrs = vals.splice(0, 3);
+          // setKeys(Object.keys(data));
+          // console.log(Object.keys(data).slice(0,3));
+          // result.push(valArrs);
+          // console.log(data["recovered"]);
+          // result.push(data["recovered"]);
+          // result.push(data["state"]);
+          valArrs.push(vals[5]);
+          valArrs.push(vals[6]);
+          result.push(valArrs);
+          // result.push(Object.values(data));
         });
 
-        setData(Object.values(result));
-        console.log(keys);
+        // setData(Object.values(result));
+        setData(result);
+        // console.log(keys);
       })
 
       .catch((e) => {
@@ -43,10 +63,11 @@ const StateWise = () => {
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="simple table">
           <TableHead>
-            <TableRow></TableRow>
-            {keys.map((val) => {
-              return <TableCell align="right">{val}</TableCell>;
-            })}
+            <TableRow style={{ background: "#6698ed" }}>
+              {keys.map((val) => {
+                return <TableCell align="right">{val}</TableCell>;
+              })}
+            </TableRow>
           </TableHead>
           <TableBody>
             {data.map((rows) => {
